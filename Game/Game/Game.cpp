957109af5ff32b3b7a79d9b4e48bec3bd647e46f
jtKidding <z59141483@gmail.com>
2019-03-12 00:42:23 +0800

    Tutorial 1

---
+++ Game/Game/Game.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace System;
+bool Guess(int number) {
+	return false;
+}
+int main(array<System::String ^> ^args)
+{
+	int i;
+	std::cin >> i;
+	if (Guess(i) == true)
+		std::cout << "YES";
+	else
+		std::cout << "NO";
+}
