05661fc2c609cdef0060cc2e22bbb040b85d848b
jtKidding <z59141483@gmail.com>
2019-03-15 11:17:19 +0800

    2

--- Game/Game/Game.cpp
+++ Game/Game/Game.cpp
@@ -3,6 +3,12 @@
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
+	static int target = -1;
+	if (target == -1) {
+		Random r;
+		target = r.Next() % 100 + 1;
+	}
+	if (number == target)return true;
 	return false;
 }
 int main(array<System::String ^> ^args)
